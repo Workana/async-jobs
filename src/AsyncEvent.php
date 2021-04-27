90c8c12cd16af4eea82806c14ec5bec97ba4aec8
Julian Paredes <julian@workana.com>
2021-04-27 11:12:37 -0300

    Add exceptions for unknown parameters

--- src/AsyncEvent.php
+++ src/AsyncEvent.php
@@ -2,46 +2,46 @@
 namespace Workana\AsyncJobs;
 use Symfony\Component\EventDispatcher\EventDispatcherInterface;
 
-/**
- * @author Carlos Frutos <charly@workana.com>
- */
 class AsyncEvent extends AsyncAction
 {
-    /**
-     * @var string $eventName
-     * @var SerializableEvent $event
-     */
-    public function __construct($eventName, SerializableEvent $event)
+    public function __construct(string $eventName, SerializableEvent $event)
     {
         parent::__construct(EventDispatcherInterface::class, 'dispatch', [$event, $eventName]);
     }
 
-    /**
-     * @return string
-     */
-    public function getEventName()
+    public function getEventName(): string
     {
         //Events produced with previous code version should have inverted parameters, so we need some logic
         //to detect how it was produced and adapt, since there will be delayed jobs.
-        $eventName = $this->parameters[1]->getValue();
-        return (is_string($eventName)) ? $eventName :  $this->parameters[0]->getValue();
+        $expectedEventNameParameter = $this->parameters[1]->getValue();
+        $fallbackParameter = $this->parameters[0]->getValue();
+
+        if (is_string($expectedEventNameParameter)) {
+            return $expectedEventNameParameter;
+        } elseif (is_string($fallbackParameter)) {
+            return $fallbackParameter;
+        } else {
+            throw new \TypeError('Event name can\'t be derived from event: ' . json_encode($this->parameters));
+        }
     }
 
-    /**
-     * @return SerializableEvent
-     */
-    public function getEvent()
+    public function getEvent(): SerializableEvent
     {
         //Events produced with previous code version should have inverted parameters, so we need some logic
         //to detect how it was produced and adapt, since there will be delayed jobs.
-        $eventData = $this->parameters[0]->getValue();
-        return ($eventData instanceof SerializableEvent) ? $eventData : $this->parameters[1]->getValue();
+        $expectedEventDataParameter = $this->parameters[0]->getValue();
+        $fallbackParameter = $this->parameters[1]->getValue();
+
+        if ($expectedEventDataParameter instanceof SerializableEvent) {
+            return $expectedEventDataParameter;
+        } elseif ($fallbackParameter instanceof SerializableEvent) {
+            return $fallbackParameter;
+        } else {
+            throw new \TypeError('Event data can\'t be derived from event: ' . json_encode($this->parameters));
+        }
     }
 
-    /**
-     * @return string
-     */
-    public function __toString()
+    public function __toString(): string
     {
         return $this->getEventName();
     }
